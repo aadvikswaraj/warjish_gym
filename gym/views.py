58de93eef2f4e03da57929e1cf2de4c3bbf0691f
Aadvik Swaraj <70912299+aadvikswaraj@users.noreply.github.com>
2024-11-18 19:54:44 +0530

    Update views.py

--- gym/views.py
+++ gym/views.py
@@ -8,9 +8,4 @@ from gym.models import notice, gallery
 def home(request):
     notices = notice.objects.filter()[:10:-1]
     photos = gallery.objects.all().reverse()
-<<<<<<< HEAD
-    print(notices)
     return render(request, template_name="index.html", context={"notices":notices, "photos":photos})
-=======
-    return render(request, template_name="index.html", context={"notices":notices, "photos":photos})
->>>>>>> 91ee0c98946464682c49b5f9e5cb38b47e9a1745
